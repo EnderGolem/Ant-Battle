f50f80b17a3860646f25b0b5101789c4997029e6
Grisha <ashgir01@gmail.com>
2025-07-11 00:44:45 +0300

    base

---
+++ Server/Manager.cs
@@ -0,0 +1,18 @@
+﻿using Server.Net;
+
+namespace Server;
+
+internal class Manager
+{
+    private readonly RestApi _api;
+    public Manager()
+    {
+        _api = new RestApi();
+    }
+
+    public  async void Cycle()
+    {
+      var posts =  await _api.GetPostsAsync();
+    }
+
+}
---
+++ Server/Net/Models/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Net.Models;
+
+public record Post
+{
+    public int UserId { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+}
