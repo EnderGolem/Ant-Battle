ebad10d17f613df209dd2ae94d71921f0ec7d5cc
Grisha <ashgir01@gmail.com>
2025-07-11 19:01:55 +0300

    first api

---
+++ Server/Net/Models/MovesRequest.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace Server.Net.Models;
+
+public class MovesRequest : Base
+{
+    [JsonProperty("moves")]
+    public IList<Move> Moves { get; set; } = new List<Move>();
+}
+
+public class Move
+{
+    [JsonProperty("ant")]
+    public string Ant { get; set; } = string.Empty;
+
+    [JsonProperty("path")]
+    public IList<Coordinate> Path { get; set; } = new List<Coordinate>();
+}
