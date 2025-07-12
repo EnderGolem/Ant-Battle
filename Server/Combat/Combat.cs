e6729f7de1bae94ed684064966df48baa96a4087
Observer189 <aleksandr.kozlovskiy03@mail.ru>
2025-07-12 09:47:28 +0300

    stuff

--- Server/Combat/Combat.cs
+++ Server/Combat/Combat.cs
@@ -6,13 +6,85 @@ public class Combat
 {
     private CombatField _combatField = new CombatField();
 
+    private List<HexCellHash> _homeCells = new List<HexCellHash>();
+
+    private HashSet<HexCellHash> _hexVisibleThisTick = new HashSet<HexCellHash>();
+
+    private Dictionary<string, Ant> _scouts = new Dictionary<string, Ant>();
+
+    private Dictionary<string, Ant> _unassignedAnts = new Dictionary<string, Ant>();
+
+    private GameState _currentGameState;
+    public CombatField CombatField => _combatField;
+
+    public List<HexCellHash> HomeCells => _homeCells;
+
+    public HashSet<HexCellHash> HexVisibleThisTick => _hexVisibleThisTick;
+
+    public GameState CurrentGameState => _currentGameState;
+
+    public Dictionary<string, Ant> Scouts => _scouts;
+
+    public Dictionary<string, Ant> UnassignedAnts => _unassignedAnts;
+
+
+    private Strategizer _strategizer;
+
     public MovesRequest Tick(GameState gameState)
     {
+        if (_homeCells.Count == 0)
+        {
+            foreach (var coord in gameState.Home)
+            {
+                _homeCells.Add(HexCellHash.FromCoordinate(coord));
+            }
+
+            _strategizer = new Strategizer(this);
+        }
+
+
+        _currentGameState = gameState;
         var input = new MovesRequest();
+        _hexVisibleThisTick.Clear();
         foreach (var tile in gameState.Map)
         {
-            _combatField.AddHexCell(new HexCellHash(tile.Q, tile.R), new HexCell(tile.Cost));
+            var hash = new HexCellHash(tile.Q, tile.R);
+            var hexCell = Encyclopedia.CreateHexCellFromType((HexType)tile.Type);
+            if (hexCell.Type == HexType.Base)
+            {
+                if(_homeCells.Contains(hash))
+                {
+                    hexCell.SetType(HexType.EnemyBase);
+                }
+            }
+
+            _hexVisibleThisTick.Add(hash);
+            _combatField.SetHexCell(hash, hexCell);
         }
+
+        foreach (var ant in gameState.Ants)
+        {
+            var stats = Encyclopedia.GetAntStatsByType(ant.Type);
+            
+            var cellsToFake = HexGridHelper.GetAllCellsInRadius(new HexCellHash(ant.Q, ant.R), stats.Speed);
+
+            foreach (var hash in cellsToFake)
+            {
+                _combatField.AddFakeCell(hash,Encyclopedia.CreateHexCellFromType(HexType.Fake));
+            }
+
+            var hexShouldBeVisible = 
+                HexGridHelper.GetAllCellsInRadius(new HexCellHash(ant.Q, ant.R), stats.Sight);
+
+            foreach (var hex in hexShouldBeVisible)
+            {
+                if (!_hexVisibleThisTick.Contains(hex))
+                {
+                    _combatField.Field[hex].SetType(HexType.EndOfMap);
+                }
+            }
+        }
+        
         
         List<Move> moves = new List<Move>();
         for (int i = 0; i < gameState.Ants.Count; i++)
@@ -33,5 +105,10 @@ public class Combat
 
 
         return input;
+    }
+
+    public void AssignAntToGroup(string id)
+    {
+        
     }
 }
---
+++ Server/Combat/Encyclopedia.cs
@@ -0,0 +1,113 @@
+﻿using Server.Net.Models;
+
+namespace Server.Combat;
+
+public static class Encyclopedia
+{
+    private static Dictionary<AntType, AntStats> _antStats;
+    
+    private static Dictionary<FoodType, FoodStats> _foodStats;
+
+    static Encyclopedia()
+    {
+        _antStats = new Dictionary<AntType, AntStats>();
+        _antStats[AntType.Worker] = new AntStats(AntType.Worker,130, 30, 8, 1, 5, 0.6f);
+        _antStats[AntType.Warrior] = new AntStats(AntType.Warrior,180, 70, 2, 1, 4, 0.3f);
+        _antStats[AntType.Scout] = new AntStats(AntType.Scout,80, 20, 2, 4, 7, 0.1f);
+
+        _foodStats = new Dictionary<FoodType, FoodStats>();
+        _foodStats[FoodType.Apple] = new FoodStats(FoodType.Apple, 10);
+        _foodStats[FoodType.Bread] = new FoodStats(FoodType.Bread, 20);
+        _foodStats[FoodType.Nectar] = new FoodStats(FoodType.Nectar, 60);
+
+    }
+
+    public static AntStats GetAntStatsByType(AntType type)
+    {
+        return _antStats[type];
+    }
+
+    public static FoodStats GetFoodStatsByType(int type)
+    {
+        return _foodStats[(FoodType)type];
+    }
+
+    public static HexCell CreateHexCellFromType(HexType type)
+    {
+        byte cost = 0;
+        bool passable = true;
+
+        switch (type)
+        {
+            case HexType.Base:
+                cost = 255;
+                passable = false;
+                break;
+            case HexType.Default:
+                cost = 1;
+                passable = true;
+                break;
+            case HexType.Dirt:
+                cost = 2;
+                passable = true;
+                break;
+            case HexType.Acid:
+                cost = 1;
+                passable = true;
+                break;
+            case HexType.Obstacle:
+                cost = 255;
+                passable = false;
+                break;
+            case HexType.EndOfMap:
+                cost = 255;
+                passable = false;
+                break;
+            case HexType.EnemyBase:
+                cost = 255;
+                passable = false;
+                break;
+            case HexType.Fake:
+                cost = 1;
+                passable = true;
+                break;
+        }
+
+        return new HexCell(type, cost, passable);
+    }
+}
+
+public class AntStats
+{
+    public AntType Type { get; }
+    public int Health { get; }
+    public int Damage { get; }
+    public int Carrying { get; }
+    public int Sight { get; }
+    public int Speed { get; }
+    public float SpawnRate { get; }
+
+    public AntStats(AntType type,int health, int damage, int carrying, int sight, int speed, float spawnRate)
+    {
+        Type = type;
+        Health = health;
+        Damage = damage;
+        Carrying = carrying;
+        Sight = sight;
+        Speed = speed;
+        SpawnRate = spawnRate;
+    }
+}
+
+public class FoodStats
+{
+    public FoodType Type { get; }
+    public int Calories { get; }
+
+    public FoodStats(FoodType type, int calories)
+    {
+        Type = type;
+        Calories = calories;
+    }
+}
+
